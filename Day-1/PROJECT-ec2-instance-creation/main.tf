f01a92d6a5c712f870ce84314785401a81015977
dranjithagowda <dranjithagowda333@gmail.com>
2026-06-13 11:26:40 +0530

    Add subnet_id to EC2 instance resource

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -5,4 +5,5 @@ provider "aws" {
 resource "aws_instance" "example" {
     ami           = "ami-0b6d9d3d33ba97d99"  # Specify an appropriate AMI ID
     instance_type = "t3.micro"
+    subnet_id = "subnet-0283e537af8b7a51c"
 }
