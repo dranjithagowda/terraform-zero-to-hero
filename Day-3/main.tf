1e9614ab51f4ea29a81079f38a4ef8d7288c0bd7
dranjithagowda <dranjithagowda333@gmail.com>
2026-06-15 05:09:23 +0530

    Update main.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -1,10 +1,1 @@
-provider "aws" {
-  region = "us-east-1"
-}
 
-module "ec2_instance" {
-  source = "./modules/ec2_instance"
-  ami_value = "ami-0521cb2d60cfbb1a6" # replace this
-  instance_type_value = "t3.micro"
-  subnet_id_value = "subnet-03465ffb835f65865" # replace this
-}
