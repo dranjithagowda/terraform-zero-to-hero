75f891773340b0c36d291ded471becc6892ef776
dranjithagowda <dranjithagowda333@gmail.com>
2026-06-15 04:55:17 +0530

    Update EC2 instance configuration values

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
-  instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+  ami_value = "ami-0521cb2d60cfbb1a6" # replace this
+  instance_type_value = "t3.micro"
+  subnet_id_value = "subnet-03465ffb835f65865" # replace this
 }
