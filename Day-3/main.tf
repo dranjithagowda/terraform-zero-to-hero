72903f4aeec2cd9069474dfd5638b137d5acc8b6
dranjithagowda <dranjithagowda333@gmail.com>
2026-06-15 05:10:37 +0530

    Update main.tf

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -6,6 +6,5 @@ module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami_value = "ami-0521cb2d60cfbb1a6" # replace this
   instance_type_value = "t3.micro"
-  subnet_id_value = "subnet-03465ffb835f65865" # replace this
 }
 
