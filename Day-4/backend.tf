6c8947b4ee8fe0b1d6a3bd17fee419fee5de8a01
dranjithagowda <dranjithagowda333@gmail.com>
2026-06-15 12:49:44 +0530

    Update backend.tf

--- Day-4/backend.tf
+++ Day-4/backend.tf
@@ -1,10 +1,9 @@
-/*terraform {
+terraform {
   backend "s3" {
-    bucket         = "abhishek-s3-demo-xyz" # change this
+    bucket         = "chinmai-s3-demo-xyz" # change this
     key            = "abhi/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
   }
 }
-*/
