c0718ee90b2c3494cd1de96b2b6e6c7a2a606951
dranjithagowda <dranjithagowda333@gmail.com>
2026-06-15 12:25:56 +0530

    Rename resources and update configurations in main.tf

--- Day-4/main.tf
+++ Day-4/main.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "abhishek" {
-  instance_type = "t2.micro"
-  ami = "ami-053b0d53c279acc90" # change this
-  subnet_id = "subnet-019ea91ed9b5252e7" # change this
+resource "aws_instance" "chinmai" {
+  instance_type = "t3.micro"
+  ami = "ami-0521cb2d60cfbb1a6" # change this
+  subnet_id = "subnet-0283e537af8b7a51c" # change this
 }
 
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "abhishek-s3-demo-xyz" # change this
+  bucket = "chinmai-s3-demo-xyz" # change this
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
